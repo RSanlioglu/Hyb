3d90dd29975c5f2209c87e5b9e43216be68fb6f6
Isak-Bamiani <90834593+Isak-Bamiani@users.noreply.github.com>
2021-10-11 19:39:46 +0200

    Laget nye filer

--- app/src/main/java/com/example/hyb/MainActivity.java
+++ app/src/main/java/com/example/hyb/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.hyb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button loginButton;
+    private Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View login = findViewById(R.id.loginButton);
+        login.setOnClickListener(this);
+
+        View register= findViewById(R.id.registerButton);
+        register.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View arg0){
+        if (arg0.getId() == R.id.loginButton) {
+            //define a new Intent for the login Activity
+            Intent intent = new Intent(this, loginActivity.class);
+
+            //start the second Activity
+            this.startActivity(intent);
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/example/hyb/loginActivity.java
@@ -0,0 +1,24 @@
+package com.example.hyb;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class loginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
