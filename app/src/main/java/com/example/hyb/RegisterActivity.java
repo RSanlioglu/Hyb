777c208e2882a8838af318369c1e992683592116
Ramazan <ramazan_sanlioglu@live.com>
2021-10-12 22:55:00 +0200

    Bruker kan nå registrere ny bruker og den vil legges inn i Firesbase sin authenticator.

--- app/src/main/java/com/example/hyb/RegisterActivity.java
+++ app/src/main/java/com/example/hyb/RegisterActivity.java
@@ -1,19 +1,63 @@
 package com.example.hyb;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "loginActivity";
+
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-
+        mAuth = FirebaseAuth.getInstance();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //Check if user is signed in
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null) {
+            Log.d(TAG, "Current user is signed in");
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        EditText txtEmailInput = findViewById(R.id.editTextEmailAddressRegister);
+        EditText txtPasswordInput = findViewById(R.id.editTextPasswordRegister);
+
+        mAuth.createUserWithEmailAndPassword(txtEmailInput.getText().toString(), txtPasswordInput.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        //TODO: Når brukeren registrere en ny bruker så kan den også legges inn i firestore.
+    }
 }
