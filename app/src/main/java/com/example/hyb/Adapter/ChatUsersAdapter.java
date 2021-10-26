c484e0e01c1545dc69af453c62aee3978bb0091f
Ramazan <ramazan_sanlioglu@live.com>
2021-10-26 17:30:19 +0200

    Viser en shabby liste med alle deltakere i et hus for å kunne chatte med dem

---
+++ app/src/main/java/com/example/hyb/Adapter/ChatUsersAdapter.java
@@ -0,0 +1,67 @@
+package com.example.hyb.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.hyb.Model.UserInfo;
+import com.example.hyb.R;
+
+import java.util.List;
+
+public class ChatUsersAdapter extends RecyclerView.Adapter<ChatUsersAdapter.ChatUsersViewHolder> {
+    private List<UserInfo> data;
+    private LayoutInflater mInflater;
+    private View.OnClickListener clickListener;
+
+    public ChatUsersAdapter(Context ctx, List<UserInfo> data, View.OnClickListener clickListener) {
+        this.data = data;
+        this.mInflater = LayoutInflater.from(ctx);
+        this.clickListener = clickListener;
+    }
+
+    @NonNull
+    @Override
+    public ChatUsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.users_item, parent, false);
+
+        return new ChatUsersViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatUsersViewHolder holder, int position) {
+        UserInfo currentUserInfo = data.get(position);
+
+        holder.bind(currentUserInfo, clickListener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    class ChatUsersViewHolder extends RecyclerView.ViewHolder {
+        TextView userName;
+        ImageView usersInitials;
+
+        public ChatUsersViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            userName = itemView.findViewById(R.id.txtUserName);
+            //usersInitials = itemView.findViewById(R.id.imageViewUserInitials);
+        }
+
+        public void bind(UserInfo currentUserInfo, View.OnClickListener clickListener) {
+            String fullName = currentUserInfo.getFirstName() + " " + currentUserInfo.getLastName();
+            userName.setText(fullName);
+
+            this.itemView.setOnClickListener(clickListener);
+        }
+    }
+}
--- app/src/main/java/com/example/hyb/ChatDisplayFragment.java
+++ app/src/main/java/com/example/hyb/ChatDisplayFragment.java
@@ -5,16 +5,35 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.hyb.Adapter.ChatUsersAdapter;
+import com.example.hyb.Model.Resident;
+import com.example.hyb.Model.UserInfo;
 import com.example.hyb.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 public class ChatDisplayFragment extends Fragment {
-    private String uidKey;  //Nøkkel for å hente bruker
+    private String uidKey, residentId;  //Nøkkel for å hente bruker
+    private UserInfo loggedInUser;
+    private Resident loggedInUserResident;
+    public ArrayList<UserInfo> residentUsers = new ArrayList<>();
+    private RecyclerView chatUsersRecyclerView;
+    private FirebaseFirestore db;
+
+    private static String TAG = "CHATDISPLAYFRAGMENT";
 
     public ChatDisplayFragment() {
         //Tom konstruktør
@@ -24,7 +43,10 @@ public class ChatDisplayFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat_display, container, false);
+        View view = inflater.inflate(R.layout.fragment_chat_display, container, false);
+
+        db = FirebaseFirestore.getInstance();
+        return view;
     }
 
     @Override
@@ -32,5 +54,56 @@ public class ChatDisplayFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         uidKey = getArguments().getString("userId");
+
+        getUserInfo(view);
+    }
+
+    private void getUserInfo(View view) {
+        DocumentReference docRef = db.collection("users").document(uidKey);
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                loggedInUser = documentSnapshot.toObject(UserInfo.class);
+                getUserResident(loggedInUser.getResidentId(), view);
+            }
+        });
+    }
+
+    private void getUserResident(String residentId, View view) {
+        DocumentReference docRef = db.collection("residents").document(residentId);
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                loggedInUserResident = documentSnapshot.toObject(Resident.class);
+                getResidentsUsers(loggedInUserResident.getOccupants(), view);
+            }
+        });
+    }
+
+    private void getResidentsUsers(ArrayList<String> occupants, View view) {
+        for(String occ : occupants) {
+            if(!occ.equals(uidKey)) {
+                DocumentReference docRef = db.collection("users").document(occ);
+                docRef.get().addOnSuccessListener(documentSnapshot -> {
+                    UserInfo receivedUser = documentSnapshot.toObject(UserInfo.class);
+                    //Log.d(TAG, "onSuccess: " + receivedUser.toString());
+                    residentUsers.add(receivedUser);
+
+                    chatUsersRecyclerView = view.findViewById(R.id.usersRecyclerView);
+
+                    chatUsersRecyclerView.setAdapter(new ChatUsersAdapter(view.getContext(), residentUsers, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            int position = chatUsersRecyclerView.getChildAdapterPosition(v);
+                            UserInfo clickedUser = residentUsers.get(position);
+                            Toast.makeText(view.getContext(), clickedUser.getFirstName() + " clicked", Toast.LENGTH_LONG).show();
+                            //TODO: Når bruker har valgt bruker dem vil chatte med så skal chat aktiviteten vises og denne skal finishes.
+                        }
+                    }));
+
+                    chatUsersRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+                });
+            }
+        }
     }
 }
