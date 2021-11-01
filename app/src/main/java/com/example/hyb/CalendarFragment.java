a487ca9cedb0c1ebd4582c34a2df98d003ee5173
Iulii Sambris <iulii.sambris@extendaretail.com>
2021-11-01 11:55:56 +0200

    Added list view for Todo
    Add list view and title for Todos

--- app/src/main/java/com/example/hyb/CalendarFragment.java
+++ app/src/main/java/com/example/hyb/CalendarFragment.java
@@ -10,6 +10,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CalendarFragment extends Fragment {
     private String uidKey; //Nøkkel for å hente bruker
@@ -30,5 +37,27 @@ public class CalendarFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         uidKey = getArguments().getString("userId");
+
+        //ListView list = view.findViewById(R.id.list);
+
+        ListView listView = (ListView) view.findViewById(R.id.list);
+
+        // Mock view
+        String[] events = new String[] {
+                "Task 1",
+                "Task 2",
+                "Task 3",
+                "Task 4",
+                "Task 5"
+        };
+
+        List<String> events_list = new ArrayList<String>(Arrays.asList(events));
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                getContext(), android.R.layout.simple_expandable_list_item_1, events_list
+        );
+        listView.setAdapter(arrayAdapter);
+        arrayAdapter.notifyDataSetChanged();
+
+        //list.
     }
 }
