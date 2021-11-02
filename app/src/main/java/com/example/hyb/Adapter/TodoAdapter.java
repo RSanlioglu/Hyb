f7a91fcea3d427c8f5eecf7bc4c2c1c3ff66ac10
violinas <violasam@users.noreply.github.com>
2021-11-02 14:01:58 +0200

    List view with title and description

--- app/src/main/java/com/example/hyb/Adapter/TodoAdapter.java
+++ app/src/main/java/com/example/hyb/Adapter/TodoAdapter.java
@@ -1,14 +1,33 @@
 package com.example.hyb.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import com.example.hyb.Model.Task;
+import com.example.hyb.R;
+
+import java.util.ArrayList;
 
 public class TodoAdapter extends BaseAdapter {
 
+    private Context ctx;
+    ArrayList<Task> tasks;
+    private LayoutInflater mInflater;
+
+    public TodoAdapter(Context ctx, ArrayList<Task> tasks){
+        this.ctx = ctx;
+        this.tasks = tasks;
+        this.mInflater = LayoutInflater.from(ctx);
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return tasks.size();
     }
 
     @Override
@@ -22,7 +41,18 @@ public class TodoAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public View getView(int i, View view, ViewGroup viewGroup)
+    {
+        view = mInflater.inflate(R.layout.activity_list, null);
+
+        CheckBox checkBox = (CheckBox) view.findViewById(R.id.checkBox2);
+
+        EditText editText = (EditText) view.findViewById(R.id.editTextTextMultiLine);
+
+        checkBox.setChecked(tasks.get(i).isCompleted());
+        checkBox.setText(tasks.get(i).getTitle());
+
+        editText.setText(tasks.get(i).getDescription());
+        return view;
     }
 }
--- app/src/main/java/com/example/hyb/TasksFragment.java
+++ app/src/main/java/com/example/hyb/TasksFragment.java
@@ -2,6 +2,7 @@ package com.example.hyb;
 
 import android.os.Bundle;
 
+import android.content.*;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -11,6 +12,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.example.hyb.Adapter.TodoAdapter;
+import com.example.hyb.Model.Task;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -36,26 +40,21 @@ public class TasksFragment extends Fragment {
 
         uidKey = getArguments().getString("userId");
 
-        //ListView list = view.findViewById(R.id.list);
-
         ListView listView = (ListView) view.findViewById(R.id.list);
 
         // Mock view
-        String[] events = new String[] {
-                "Task 11",
-                "Task 2",
-                "Task 3",
-                "Task 4",
-                "Task 5"
-        };
+        ArrayList<Task> tasks = new ArrayList<Task>();
+        Task t = new Task();
+        t.setTitle("Title");
+        t.setDescription("Task description");
+        t.setCompleted(false);
 
-        List<String> events_list = new ArrayList<String>(Arrays.asList(events));
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                getContext(), android.R.layout.simple_expandable_list_item_1, events_list
-        );
-        listView.setAdapter(arrayAdapter);
-        arrayAdapter.notifyDataSetChanged();
+        for (int i =0 ; i< 10; ++i)
+            tasks.add(t);
 
-        //list.
+        TodoAdapter adapter = new TodoAdapter( getActivity(), tasks );
+        listView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
     }
 }
