3b1cc8d8fada51569cb5a7b4ff1663f38ed286c2
Ramazan <ramazan_sanlioglu@live.com>
2021-11-02 13:46:08 +0100

    Lagt til tooblar for SettingsActivity

--- app/src/main/java/com/example/hyb/SettingsActivity.java
+++ app/src/main/java/com/example/hyb/SettingsActivity.java
@@ -1,14 +1,59 @@
 package com.example.hyb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingsActivity extends AppCompatActivity {
+    public String userUid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Intent receivedIntent = getIntent();
+        userUid = receivedIntent.getStringExtra(RegisterActivity.KEY_NAME);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_dashboard);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.appbar_menu_light, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                Intent intentLogout = new Intent(this, MainActivity.class);
+                intentLogout.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                getApplicationContext().startActivity(intentLogout);
+                finish();
+                Toast.makeText(this, "Signed out", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.settings:
+                Intent intentSettings = new Intent(this, SettingsActivity.class);
+                intentSettings.putExtra("userUid", userUid);
+                intentSettings.addFlags((Intent.FLAG_ACTIVITY_NEW_TASK));
+                getApplicationContext().startActivity(intentSettings);
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
