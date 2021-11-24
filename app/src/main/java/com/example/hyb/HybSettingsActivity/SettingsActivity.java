3bb3beca1cd39bf4058dd7796f01d80a35da136d
Ramazan <ramazan_sanlioglu@live.com>
2021-11-24 23:06:44 +0100

    Settings fikset

--- app/src/main/java/com/example/hyb/HybSettingsActivity/SettingsActivity.java
+++ app/src/main/java/com/example/hyb/HybSettingsActivity/SettingsActivity.java
@@ -1,12 +1,6 @@
 package com.example.hyb.HybSettingsActivity;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -14,13 +8,26 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuCompat;
+
 import com.example.hyb.MainActivity;
+import com.example.hyb.Model.Resident;
+import com.example.hyb.Model.UserInfo;
 import com.example.hyb.R;
-import com.example.hyb.RegisterActivity;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Random;
 
 public class SettingsActivity extends AppCompatActivity {
     public String userUid;
+    public FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,23 +35,17 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         Intent receivedIntent = getIntent();
         userUid = receivedIntent.getStringExtra("userUid");
+        db = FirebaseFirestore.getInstance();
 
         Toolbar toolbar = findViewById(R.id.toolbar_dashboard);
         setSupportActionBar(toolbar);
-
-        /*Fragment startFragment = new SettingsOperationFragment();
-        Bundle arguments = new Bundle();
-        arguments.putString("userUid", userUid);
-        startFragment.setArguments(arguments);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView2, startFragment).commit();*/
     }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.appbar_menu_light, menu);
+        MenuCompat.setGroupDividerEnabled(menu, true);
         return true;
     }
 
@@ -65,9 +66,69 @@ public class SettingsActivity extends AppCompatActivity {
                 getApplicationContext().startActivity(intentSettings);
                 finish();
                 return true;
+            case R.id.leave:
+                AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
+                builder.setMessage("This will remove you from the resident. If you are the only member of this resident the resident will also be deleted").setTitle("Are you sure?");
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        leaveResident();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        //Nothing happens if they cancel
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void leaveResident() {
+        //Henter brukeren
+        DocumentReference userRef = db.collection("users").document(userUid);
+        userRef.get().addOnSuccessListener(userSnapshot -> {
+            UserInfo user = userSnapshot.toObject(UserInfo.class);
+            String residentId = user.getResidentId();
+            //Setter resident null på brukeren som forlater
+            userRef.update("residentId", null).addOnSuccessListener(s -> {
+                //Henter resident og fjerner brukeren som occupant
+                DocumentReference residentRef = db.collection("residents").document(residentId);
+                residentRef.update("occupants", FieldValue.arrayRemove(userUid)).addOnSuccessListener(r -> {
+                    //Henter oppdatert resident
+                    DocumentReference updatedResident = db.collection("residents").document(residentId);
+                    updatedResident.get().addOnSuccessListener(residentSnapshot -> {
+                        Resident resident = residentSnapshot.toObject(Resident.class);
+                        if(resident.getOccupants().size() <= 0) {
+                            db.collection("residents").document(residentId).delete().addOnSuccessListener(unused -> navigateOut());
+                        } else if(resident.getHost().equals(userUid)) {
+                            //If the host leaves a random new host is selected
+                            Random r1 = new Random();
+                            int randomNum = r1.nextInt(resident.getOccupants().size());
+                            updatedResident.update("host", resident.getOccupants().get(randomNum)).addOnSuccessListener(unused -> navigateOut());
+                        } else {
+                            navigateOut();
+                        }
+                    });
+
+                });
+            });
+        });
+    }
+
+    private void navigateOut() {
+        Intent intent = new Intent(getBaseContext(), MainActivity.class);
+        intent.putExtra("UserInfo", userUid);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        getBaseContext().startActivity(intent);
+        finishAffinity(); //Android 4.1 or higher
+        Toast.makeText(getBaseContext(), "You successfully left the resident", Toast.LENGTH_SHORT).show();
     }
 
     /**
